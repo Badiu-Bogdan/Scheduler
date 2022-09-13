6e1b5b88cdf7ec6a1a73718bd6e17cba9bee13b3
Bogdan <bbogdan@ymail.com>
2022-09-13 17:54:58 +0300

    reconfiguring the project to use jetpack compose with material3

--- app/src/main/java/com/example/scheduler/MainActivity.kt
+++ app/src/main/java/com/example/scheduler/MainActivity.kt
@@ -1,16 +1,47 @@
 package com.example.scheduler
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.scheduler.ui.theme.SchedulerTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        installSplashScreen().apply {
+        installSplashScreen().apply {}
 
+        setContent {
+            SchedulerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
         }
-        setContentView(R.layout.activity_main)
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    SchedulerTheme {
+        Greeting("Android")
     }
 }
