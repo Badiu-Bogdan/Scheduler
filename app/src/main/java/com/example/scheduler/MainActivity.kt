a51120c0ad926dd9616f43eb344516dcedf08dbf
Bogdan <bbogdan@ymail.com>
2022-09-13 18:04:19 +0300

    trying to reconfigure the app to run

--- app/src/main/java/com/example/scheduler/MainActivity.kt
+++ app/src/main/java/com/example/scheduler/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.scheduler
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -15,9 +16,15 @@ import com.example.scheduler.ui.theme.SchedulerTheme
 
 class MainActivity : ComponentActivity() {
 
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        installSplashScreen().apply {}
+        installSplashScreen().apply {
+            this.setKeepOnScreenCondition{
+                viewModel.isLoading.value
+            }
+        }
 
         setContent {
             SchedulerTheme {
---
+++ app/src/main/java/com/example/scheduler/MainViewModel.kt
@@ -0,0 +1,21 @@
+package com.example.scheduler
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class MainViewModel: ViewModel() {
+    private val _isLoading = MutableStateFlow(true)
+    val isLoading = _isLoading.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            //Here i can do some computations while this is waiting
+            delay(3000)
+            _isLoading.value = false
+        }
+    }
+}
